cd25f81cc071ee5b1b6e1fd008ec500d33d0dc7b
Adam Bubenicek <adam@adambubenicek.com>
2023-10-11 13:22:24 +0200

    Hide Firefox view

--- firefox/user.js
+++ firefox/user.js
@@ -1,4 +1,5 @@
 user_pref("signon.rememberSignons", false);
 user_pref("middlemouse.paste", false);
 user_pref("browser.toolbars.bookmarks.visibility", "never");
+user_pref("browser.tabs.firefox-view", false);
 
